a8123f4dbb9081cc7da6805a228f387d9dbd1d49
mohammed <mohamedramadan2585@gmail.com>
2023-08-29 15:50:19 +0300

    fix navbar link problem

--- src/Components/Pages/Home/Navbar/NavLinks.js
+++ src/Components/Pages/Home/Navbar/NavLinks.js
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 function NavLinks(props) {
   return (
     <>
-      <li className={props.className} id={props.id} onClick={props.changeColor}>
-        <Link to={props.link}>{props.name}</Link>
-      </li>
+      <Link
+        to={props.link}
+        className={props.className}
+        id={props.id}
+        onClick={props.changeColor}
+      >
+        <a href={props.link}>{props.name}</a>
+      </Link>
     </>
   );
 }
